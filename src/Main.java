bb44c30ac53a7f21ed67f190feb37e83880847f2
devJhonatanL <jhonatan.profissional123@gmail.com>
2024-12-01 14:59:46 -0300

    comentarios

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,121 @@
-package PACKAGE_NAME;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.Select;
 
 public class Main {
+    public static void main(String[] args) throws InterruptedException {
+//Adicione o tipo de driver. No nosso caso, chrome driver
+//Adicione o caminho em que esta o ChromeDriver
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\Jhonatan\\Desktop\\chromedriver-win64\\chromedriver.exe");
+        ChromeOptions ops = new ChromeOptions();
+        ops.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(ops);
+        driver.get("https://www.saucedemo.com/");
+
+        Thread.sleep(2000);
+
+
+        //LOGIN
+        WebElement nome = driver.findElement(By.id("user-name"));
+        nome.sendKeys("standard_user");
+        Thread.sleep(2000);
+
+        WebElement senha = driver.findElement(By.id("password"));
+        senha.sendKeys("secret_sauce");
+
+        Thread.sleep(2000);
+
+        WebElement botao = driver.findElement(By.id("login-button"));
+        botao.click();
+
+        Thread.sleep(3000);
+
+
+        //Acessando uma aba do produto, add no carrinho e voltando pra home
+        driver.findElement(By.linkText("Sauce Labs Onesie")).click();
+        Thread.sleep(2000);
+
+        WebElement compra1 = driver.findElement(By.id("add-to-cart"));
+        compra1.click();
+        Thread.sleep(2000);
+
+        WebElement back1 = driver.findElement(By.id("back-to-products"));
+        back1.click();
+
+        Thread.sleep(2000);
+
+        driver.findElement(By.linkText("Sauce Labs Fleece Jacket")).click();
+        Thread.sleep(3000);
+
+        WebElement compra2 = driver.findElement(By.id("add-to-cart"));
+        compra2.click();
+        Thread.sleep(3000);
+
+        WebElement back2 = driver.findElement(By.id("back-to-products"));
+        back2.click();
+
+        Thread.sleep(3000);
+
+        //add no carrinho na home
+        WebElement compra3 = driver.findElement(By.id("add-to-cart-sauce-labs-backpack"));
+        compra3.click();
+
+        Thread.sleep(3000);
+
+        //Indo pro carrinho
+        WebElement carrinho = driver.findElement(By.id("shopping_cart_container"));
+        carrinho.click();
+
+        Thread.sleep(3000);
+
+        //Finalizando a compra
+        WebElement checkout = driver.findElement(By.id("checkout"));
+        checkout.click();
+        Thread.sleep(3000);
+
+        WebElement primeironome = driver.findElement(By.id("first-name"));
+        primeironome.sendKeys("Eliesio");
+        Thread.sleep(3000);
+
+        WebElement sobrenome = driver.findElement(By.id("last-name"));
+        sobrenome.sendKeys("Xavier");
+        Thread.sleep(3000);
+
+        WebElement cep = driver.findElement(By.id("postal-code"));
+        cep.sendKeys("76382685");
+        Thread.sleep(3000);
+
+
+        WebElement continuar = driver.findElement(By.id("continue"));
+        continuar.click();
+        Thread.sleep(3000);
+
+        WebElement finalizar = driver.findElement(By.id("finish"));
+        finalizar.click();
+        Thread.sleep(3000);
+
+
+        //Voltando pro menu e saindo
+        WebElement voltar = driver.findElement(By.id("back-to-products"));
+        voltar.click();
+        Thread.sleep(3000);
+
+        WebElement menu = driver.findElement(By.id("react-burger-menu-btn"));
+        menu.click();
+        Thread.sleep(3000);
+
+        WebElement sair = driver.findElement(By.id("logout_sidebar_link"));
+        sair.click();
+        Thread.sleep(4000);
+
+        //fecha tudo
+        driver.quit();
+
+    }
+
+
+
 }
